637336011b81debdc217854ff82d3a3548691c88
Alex Robson <asrobson@gmail.com>
2012-08-24 19:04:45 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,11 @@
+## Anvil Coffee Plugin
+
+This plugin requires anvil.js version 0.8.* or greater.
+
+## Installation
+
+	anvil install anvil.jade
+
+## Usage
+
+If this plugin is installed and enabled, it will automatically 'compile' files ending in .jade and output .html files in your output folder.
---
+++ src/plugin.js
@@ -0,0 +1,29 @@
+var jade = require( "jade" );
+
+var coffeeCompilerFactory = function( _, anvil ) {
+	return anvil.plugin( {
+		name: "anvil.jade",
+		
+		configure: function( config, command, done ) {
+			anvil.addCompiler( ".jade", this );
+			done();
+		},
+
+		compile: function( content, done ) {
+			try {
+				var compile = jade.compile( content, {
+					pretty: true
+				} );
+				done( compile({}) );
+			} catch ( error ) {
+				done( "", error );
+			}
+		},
+
+		rename: function( name ) {
+			return name.replace( ".jade", ".html" );
+		}
+	} );
+};
+
+module.exports = coffeeCompilerFactory;
